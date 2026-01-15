db74f5736e68fe0239605261f0ee6ba181eb073d
Tomáš Homola <xhomola1@fi.muni.cz>
2026-01-15 20:31:07 +0100

    feat:currency change with hardcoded exchange rates

---
+++ src/MusicoStore.BusinessLayer/Services/ICurrencyConversionService.cs
@@ -0,0 +1,8 @@
+using MusicoStore.DataAccessLayer.Enums;
+
+namespace MusicoStore.BusinessLayer.Services;
+
+public interface ICurrencyConversionService
+{
+    decimal Convert(decimal amount, Currency fromCurrency, Currency toCurrency);
+}
